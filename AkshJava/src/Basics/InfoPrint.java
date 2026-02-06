38e90318b0954dd8e514f59de57c306802263aec
Akshaya Vemula <akshayavemula17@gmail.com>
2026-02-06 19:58:34 +0530

    First commit

---
+++ AkshJava/src/Basics/InfoPrint.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class InfoPrint {
+	 public static void main(String[] args) {
+	    System.out.println("Name : Akshaya");
+		System.out.println("Age : 18");
+		System.out.println("College : Panimalar");
+
+	    }
+
+}
